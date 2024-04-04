d522b266f0c20856f45d5dd1d4bd6ce9a6ec9bee
Nicolas Angel Risso Fontan <nicolasangelrissofontan@MBP-de-Nicolas.fibertel.com.ar>
2024-04-04 19:45:36 -0300

    Prueba de sonar

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-chrome_driver_path = './drivers/chrome/chromedriver'
+chrome_driver_path = './drivers/chrome/chromedriverMac'
 
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service)
