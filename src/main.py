07ebb976c7b2242df1d23d3cf5ba84154df4dbf7
Nicolas Angel Risso Fontan <nicolasangelrissofontan@MBP-de-Nicolas.fibertel.com.ar>
2024-04-08 10:57:26 -0300

    update main

--- src/main.py
+++ src/main.py
@@ -2,8 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless")
 # Configurar el controlador del navegador Chrome
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
+
 
 url = "https://www.saucedemo.com/"
 driver.get(url)
